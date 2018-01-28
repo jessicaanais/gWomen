1384810191a84d7681fb6906b92afcd049e6ed47
Jessica Anais Duarte Cabrera <duarte.jezzica@gmail.com>
2018-01-27 21:42:34 -0700

    changed user input

--- Backend/db/migrations/20180120125149_users.js
+++ Backend/db/migrations/20180120125149_users.js
@@ -4,12 +4,11 @@ exports.up = function(knex, Promise) {
     table.increments();
     table.string('firstname');
     table.string('lastname');
-    table.string('email');
+    table.string('username');
     table.string('password');
     table.string('photo');
     table.string('about');
     table.string('cohort');
-    table.string('city');
     table.string('state');
   })
 };
