f58980d04a91052850b3caa0a54a247dd0c7a55d
Jessica Anais Duarte Cabrera <duarte.jezzica@gmail.com>
2018-01-30 21:54:42 -0700

    added user page

--- gWomen/src/app/app.module.ts
+++ gWomen/src/app/app.module.ts
@@ -46,7 +46,6 @@ const routeLists : Routes = [
     FilterstatePipe,
     FiltertitlePipe,
     FiltertitlePipe
-    // FilterjobsPipe,
   ],
   imports: [
     BrowserModule,
--- gWomen/src/app/users/users.component.ts
+++ gWomen/src/app/users/users.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent{
+  private apiURL = 'http://localhost:8000/users';
+  data: any = [{}];
 
-  constructor() { }
+  constructor(private http: Http){
+    this.getUsers();
+  }
+  getData(){
+    return this.http.get(this.apiURL)
+    .map((res: Response) => res.json())
+  }
 
-  ngOnInit() {
+  getUsers(){
+    this.getData().subscribe(data => {
+      this.data = data
+      console.log("users", data)
+    })
   }
 
 }
